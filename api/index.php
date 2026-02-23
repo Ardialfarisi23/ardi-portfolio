436f508dd4a9fd4be096335f8d89a1d480e76575
Ardi Alfarisi <ardialfarisi27@gmail.com>
2026-02-23 22:11:16 +0700

    Final Fix: Manual autoloader and temporary cache path

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
+// Memuat Autoloader Composer secara manual
+require __DIR__ . '/../vendor/autoload.php';
+
+// Memuat aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Menjalankan aplikasi
 require __DIR__ . '/../public/index.php';
