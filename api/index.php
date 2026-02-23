ecb3c4c952838023d7161c1b2b0e8de3d277a7ad
Ardi Alfarisi <ardialfarisi27@gmail.com>
2026-02-24 00:16:02 +0700

    Final fix: force https, isolate DB, and set vite output

--- api/index.php
+++ api/index.php
@@ -17,15 +17,15 @@ foreach ($paths as $path) {
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Cara yang benar untuk memindahkan storage ke /tmp
+// Memaksa Laravel menggunakan folder /tmp untuk storage
 $app->useStoragePath('/tmp/storage');
 
-// Mengatur path view yang sudah di-compile melalui config
-$app->make('config')->set('view.compiled', '/tmp/storage/framework/views');
-
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
+
 $response->send();
+
 $kernel->terminate($request, $response);
